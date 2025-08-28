f8da9910c1e203d5483bfdcaed21dac0c4dc58ed
mhann4 <mhannlayy4@gmail.com>
2025-08-28 23:17:08 +0700

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,4 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
+Document.addEventListener("DOMContentLoaded", () => {
   const checkbox = document.getElementById("toggleCheckbox");
   checkbox.checked = true; // Auto-open menu on page load
+});
+
+window.addEventListener('popstate', function(event) {
+  // Check if we're on the main menu page. If so, exit the app.
+  if (window.location.href.includes('menu.html')) {
+    window.close(); // or history.go(-1);
+  }
 });
