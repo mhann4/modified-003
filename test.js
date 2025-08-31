1550bec9e0c67e0ec3df766874b2cbca83b3e112
mhann4 <mhannlayy4@gmail.com>
2025-08-31 23:33:48 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,17 +3,36 @@ document.getElementById('matchCard').addEventListener('click', function() {
   const matchDetails = document.querySelector('.match-details');
   const qualityOptions = document.getElementById('qualityOptions');
   
-  // Hide initial view and show quality options
   matchInfo.style.display = 'none';
   matchDetails.style.display = 'none';
   qualityOptions.style.display = 'block';
 });
 
-function loadVideo() {
+function loadVideo(quality) {
   const matchCard = document.getElementById('matchCard');
   const videoContainer = document.getElementById('videoContainer');
+  const videoFrame = document.getElementById('videoFrame');
   
-  // Hide the match card and show the video container
+  let videoSrc = '';
+  if (quality === 'hd') {
+    videoSrc = 'https://okwintv.vip/livestream/12391';
+  } else if (quality === 'sd') {
+    videoSrc = 'https://www.iamchanelwestcoast.com/truc-tiep/liverpool-vs-arsenal-31-08-2025/';
+  }
+
+  videoFrame.src = videoSrc;
+  
   matchCard.style.display = 'none';
   videoContainer.style.display = 'block';
+}
+
+function goFullscreen() {
+  const videoFrame = document.getElementById('videoFrame');
+  if (videoFrame.requestFullscreen) {
+    videoFrame.requestFullscreen();
+  } else if (videoFrame.webkitRequestFullscreen) { /* Safari */
+    videoFrame.webkitRequestFullscreen();
+  } else if (videoFrame.msRequestFullscreen) { /* IE11 */
+    videoFrame.msRequestFullscreen();
+  }
 }
