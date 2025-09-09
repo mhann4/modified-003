d82a3489a7c5c0392a5244c1a70fab67fc8cf320
mhann4 <mhannlayy4@gmail.com>
2025-09-10 06:50:46 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,38 +1,27 @@
-document.getElementById('matchCard').addEventListener('click', function() {
-  const matchInfo = document.getElementById('matchInfo');
-  const matchDetails = document.querySelector('.match-details');
-  const qualityOptions = document.getElementById('qualityOptions');
-  
-  matchInfo.style.display = 'none';
-  matchDetails.style.display = 'none';
-  qualityOptions.style.display = 'block';
-});
-
-function loadVideo(quality) {
-  const matchCard = document.getElementById('matchCard');
-  const videoContainer = document.getElementById('videoContainer');
-  const videoFrame = document.getElementById('videoFrame');
-  
-  let videoSrc = '';
-  if (quality === 'hd') {
-    videoSrc = 'https://okwintv.vip/livestream/12391';
-  } else if (quality === 'sd') {
-    videoSrc = 'https://www.iamchanelwestcoast.com/truc-tiep/liverpool-vs-arsenal-31-08-2025/';
-  }
-
-  videoFrame.src = videoSrc;
-  
-  matchCard.style.display = 'none';
-  videoContainer.style.display = 'block';
+// Function to initialize Google Translate Element
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'my', includedLanguages: 'en'}, 'google_translate_element');
 }
 
-function goFullscreen() {
-  const videoFrame = document.getElementById('videoFrame');
-  if (videoFrame.requestFullscreen) {
-    videoFrame.requestFullscreen();
-  } else if (videoFrame.webkitRequestFullscreen) { /* Safari */
-    videoFrame.webkitRequestFullscreen();
-  } else if (videoFrame.msRequestFullscreen) { /* IE11 */
-    videoFrame.msRequestFullscreen();
+// Function to trigger the translation to English
+function translatePageToEnglish() {
+  const translateElement = document.querySelector('.goog-te-combo');
+  if (translateElement) {
+    translateElement.value = 'en';
+    translateElement.dispatchEvent(new Event('change'));
   }
 }
+
+// Automatically translate the page to English when it loads
+// We use a slight delay to ensure the translate script has loaded
+window.onload = function() {
+  setTimeout(translatePageToEnglish, 1000); // 1 second delay
+};
+
+// Add the Google Translate script to the body
+(function() {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+  document.body.appendChild(script);
+})();
