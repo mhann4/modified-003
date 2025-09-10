25375789dbcf33eb4895403f2738288d5a4e5265
mhann4 <mhannlayy4@gmail.com>
2025-09-10 07:22:32 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,31 +1,3 @@
-// Function to load the Google Translate script
-function loadGoogleTranslate() {
-  const script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-  document.body.appendChild(script);
-}
-
-// Function to initialize Google Translate Element and select English
-function googleTranslateElementInit() {
-  new google.translate.TranslateElement({
-    pageLanguage: 'my',
-    includedLanguages: 'en',
-    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
-  }, 'google_translate_element');
-  
-  // Wait for the dropdown to be created and then set the value to English
-  const interval = setInterval(() => {
-    const selectElement = document.querySelector('.goog-te-combo');
-    if (selectElement) {
-      selectElement.value = 'en';
-      selectElement.dispatchEvent(new Event('change'));
-      clearInterval(interval);
-    }
-  }, 500);
-}
-
-// Function to handle the buttons and load the stream
 function loadStream(quality) {
   const mainContainer = document.querySelector('.main-container');
   const streamContainer = document.getElementById('stream-container');
@@ -45,4 +17,28 @@ function loadStream(quality) {
   
   // Now, load the Google Translate element
   loadGoogleTranslate();
+}
+
+function loadGoogleTranslate() {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+  document.body.appendChild(script);
+}
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'my',
+    includedLanguages: 'en',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+  }, 'google_translate_element');
+  
+  const interval = setInterval(() => {
+    const selectElement = document.querySelector('.goog-te-combo');
+    if (selectElement) {
+      selectElement.value = 'en';
+      selectElement.dispatchEvent(new Event('change'));
+      clearInterval(interval);
+    }
+  }, 500);
 }
