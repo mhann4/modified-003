0ca44637b1ddab638fa41ca19c435622a935ab2f
mhann4 <mhannlayy4@gmail.com>
2025-09-10 08:36:38 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', function(event) {
+  // This service worker does nothing special, it just fetches the requested resource.
+  // Caching is not used, so the app will not work offline.
+});
